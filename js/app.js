eefe219d8b88bc4b82357ecf288d56e521b31350
Andrea Tomasoni <andrea96.t@gmail.com>
2017-12-15 02:55:15 +0100

    Space shuttle movement

--- js/app.js
+++ js/app.js
@@ -4,11 +4,37 @@ var context = canvas.getContext('2d');
 var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 
+// controls
+window.addEventListener('keydown', function(event) {
+    var key = keyMap[event.keyCode];
+    pressedKeys[key] = true;
+});
+window.addEventListener('keyup', function(event) {
+    var key = keyMap[event.keyCode];
+    pressedKeys[key] = false;
+});
+var keyMap = {
+    38: 'up', 87: 'up',
+    39: 'right', 68: 'right',
+    40: 'down', 83: 'down',
+    37: 'left', 65: 'left',
+    32: 'spacebar'
+};
+var pressedKeys = {
+    up: false,
+    left: false,
+    right: false,
+    down: false,
+    spacebar: false
+};
+
+var spaceShuttle = new SpaceShuttle(canvasWidth, canvasHeight);
+
 // ============
 // == UPDATE ==
 // ============
 function update(delta) {
-    console.log('update');
+    spaceShuttle.update(pressedKeys);
 }
 
 // ============
@@ -17,7 +43,7 @@ function update(delta) {
 function render() {
     // clear the canvas
     context.clearRect(0, 0, canvasWidth, canvasHeight);
-    console.log('render');
+    spaceShuttle.render(context);
 }
 
 // ===============
---
+++ js/space_shuttle.js
@@ -0,0 +1,67 @@
+var SIZE = 20;
+
+var MAX_SPEED = 6;
+var FRICTION = 0.92;
+
+function SpaceShuttle(canvasWidth, canvasHeight) {
+    // dimension
+    this.width = SIZE;
+    this.height = SIZE;
+    // position
+    this.x = (canvasWidth - this.width) / 2;
+    this.y = (canvasHeight - this.height) / 2;
+    // velocity
+    this.velX = 0;
+    this.velY = 0;
+
+    // movement
+    this.update = function(pressedKeys) {
+        // check which direction to move
+        if (pressedKeys.up) {
+            if (this.velY > -MAX_SPEED) {
+                this.velY--;
+            }
+        }
+        if (pressedKeys.down) {
+            if (this.velY < MAX_SPEED) {
+                this.velY++;
+            }
+        }
+        if (pressedKeys.right) {
+            if (this.velX < MAX_SPEED) {
+                this.velX++;
+            }
+        }
+        if (pressedKeys.left) {
+            if (this.velX > -MAX_SPEED) {
+                this.velX--;
+            }
+        }
+
+        // apply friction
+        this.velX *= FRICTION;
+        this.velY *= FRICTION;
+        this.x += this.velX;
+        this.y += this.velY;
+
+        // screen bounds
+        if (this.x <= 0) {
+            this.x = 0;
+        }
+        else if (this.x + this.width >= canvasWidth) {
+            this.x = canvasWidth - this.width;
+        }
+        if (this.y <= 0) {
+            this.y = 0;
+        }
+        else if (this.y + this.height >= canvasHeight) {
+            this.y = canvasHeight - this.height;
+        }
+    };
+
+    // draw
+    this.render = function(context) {
+        context.fillStyle = '#f00';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    };
+}
